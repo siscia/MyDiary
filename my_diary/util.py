5457a728709e5d488acb36ced3d2e54301746a47
Simone Mosciatti <simo@localhost.localdomain>
2013-05-21 22:22:09 +0200

    fixed pic name problem

--- my_diary/util.py
+++ my_diary/util.py
@@ -80,7 +80,7 @@ type_to_fields = {
     "status_like" : ["from", "name", "picture", "link"],
 
     #New ones
-    "photos" : ["from","source","created_time","tags","comments.fields(message,id,from)","likes.fields(pic_square,name)"],
+    "photos" : ["from","source","created_time","tags","comments.fields(message,id,from)","likes.fields(pic_square,name)","name"],
     "videos" : ["from","link","picture","name", "description","likes.fields(pic_square,name)","comments.fields(message,id,from)","created_time", "source", "tags"],
     "links" : ["from", "message","picture","link","created_time","comments.fields(from,message)","description",],
     "albums" : ["photos.fields(id)","id","count","created_time","likes.fields(pic_square,name)","comments", "name", "cover_photo", "place", "from",],
